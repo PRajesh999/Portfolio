a064d58649444248c6cb714e6cab5f33fad57292
rajesh <pnrajesh642@gmail.com>
2025-02-24 15:40:28 +0530

    Done

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -3,7 +3,7 @@ import blue from "../assets/svg/about/blueTheme-boy.svg"
 
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Rajesh. I am a dedicated Front-End Web Developer with expertise in HTML, CSS, JavaScript, and React.",
+    description1: "My name's Narasimha Rajesh Pulugurtha. I am a dedicated Front-End Web Developer with expertise in HTML, CSS, JavaScript, and React.",
     description2: "I am also proficient in programming languages like C, C++, and Python, with a solid understanding of Data Structures and Algorithms. My skills allows me to develop responsive web applications with efficiency.",
     image: 1
 };
